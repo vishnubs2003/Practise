3beb3c330b747805a7ab6f12a2a7aacbd5b5f9b0
Vishnu <bsvishnu1000@gmail.com>
2026-06-25 22:05:58 +0530

    update2

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import fs from "fs"
- let data=fs.readFileSync('demo.txt','utf-8',(err,data)=>{
+//  let data=fs.readFileSync('demo.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.log("error");
+        
+//     }else{
+//         console.log(data);
+        
+//     }
+//  })
+//  console.log(data);
+//   let data1=fs.writeFileSync('vis.txt',"helllo vishnu")
+ let data2=fs.appendFile("vis.txt","\ngood morning",(err)=>{
     if(err){
         console.log("error");
         
     }else{
-        console.log(data);
+        console.log("data append successfully");
         
     }
  })
- console.log(data);
-  let data1=fs.writeFileSync('vis.txt',"helllo vishnu")
