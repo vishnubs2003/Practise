89bcda11aa75025331ee01dd7d4956378b927685
Vishnu <bsvishnu1000@gmail.com>
2026-06-25 22:10:31 +0530

    update3

--- app.js
+++ app.js
@@ -8,14 +8,27 @@ import fs from "fs"
         
 //     }
 //  })
-//  console.log(data);
-//   let data1=fs.writeFileSync('vis.txt',"helllo vishnu")
- let data2=fs.appendFile("vis.txt","\ngood morning",(err)=>{
-    if(err){
-        console.log("error");
+// //  console.log(data);
+// //   let data1=fs.writeFileSync('vis.txt',"helllo vishnu")
+//  let data2=fs.appendFile("vis.txt","\ngood morning",(err)=>{
+//     if(err){
+//         console.log("error");
         
-    }else{
-        console.log("data append successfully");
+//     }else{
+//         console.log("data append successfully");
         
-    }
- })
+//     }
+//  })
+
+async function write(){
+    let d=await fs.readFile('vis.txt','utf-8',(err,data)=>{
+        if(err){
+            console.log("error")
+            
+        }else{
+            console.log(data);
+            
+        }
+    })
+}
+write()
