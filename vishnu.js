24b3de1e9713d5fa0ec10ee551522ae14fc3fb12
Vishnu <bsvishnu1000@gmail.com>
2026-06-27 22:15:15 +0530

    create server

--- vishnu.js
+++ vishnu.js
@@ -1,1 +1,10 @@
-console.log("hello vishnu");
+import http from 'http'
+ const port=3000;
+let data =http.createServer((req,res)=>{
+    res.end("welcome to server");
+})
+data.listen(port,()=>{
+    console.log(`server is running in the port ${port}`);
+    
+})
+
