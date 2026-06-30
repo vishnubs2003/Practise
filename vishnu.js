f1320e9ad5de2ad07ebb5b181e43e8ce1e6d49d9
Vishnu <bsvishnu1000@gmail.com>
2026-06-30 21:34:09 +0530

    un cmnt

--- vishnu.js
+++ vishnu.js
@@ -1,14 +1,14 @@
 // import http from 'http'
 import fs from 'fs'
 //  const port=3000;
-// let data =http.createServer((req,res)=>{
-//     res.end("welcome to server");
-//     res.end(add())
-// })
-// data.listen(port,()=>{
-//     console.log(`server is running in the port ${port}`);
+let data =http.createServer((req,res)=>{
+    res.end("welcome to server");
+    res.end(add())
+})
+data.listen(port,()=>{
+    console.log(`server is running in the port ${port}`);
     
-// })
+})
 
 
  async function add(){
